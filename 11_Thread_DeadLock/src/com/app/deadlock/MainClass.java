e9e98518e83a45fc3f4c79b4ddfc3715376f9073
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-06-15 23:51:56 +0530

    Deadlock solution

--- 11_Thread_DeadLock/src/com/app/deadlock/MainClass.java
+++ 11_Thread_DeadLock/src/com/app/deadlock/MainClass.java
@@ -7,6 +7,7 @@ public class MainClass {
 		Object ob2=new Object();
 		ThreadTest1 t1=new ThreadTest1(ob1,ob2);
 		ThreadTest2 t2=new ThreadTest2(ob1,ob2);
+		//ThreadTest2_DeadLockSolution t2=new ThreadTest2_DeadLockSolution(ob1,ob2); // same order of obj lock 
 		t1.start();
 		t2.start();
 		
